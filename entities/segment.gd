ae0c3dadef1bef85bb757fe9e8fbbca9a3eaff9f
Marek Kotewicz <marek.kotewicz@gmail.com>
2025-03-04 19:13:47 +0800

    example animation

--- entities/segment.gd
+++ entities/segment.gd
@@ -16,22 +16,7 @@ class_name Segment
 		if is_node_ready():
 			get_parent().queue_redraw()
 
-@export var should_draw: bool:
-	set(value):
-		should_draw = value
-		queue_redraw()
-
-func update_segment(prev: Segment):
-	var angle = atan2(prev.position.y - position.y, prev.position.x - position.x)
-	rotation = angle
-
-	var direction = position.direction_to(prev.position)
-	position = prev.position - direction * distance_constraint
-
 func _draw() -> void:
-	if !should_draw:
-		return
-
 	var parent = get_parent()
 	if "drawer" in parent:
 		parent.drawer._draw_segment(self)
